dc11cf9eb2e764e21736ff6e7ef411c8d8c67f6c
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-04-29 21:09:19 -0400

    Improved fetcher to handle multiple dropped resources

--- roleFetcher.js
+++ roleFetcher.js
@@ -5,13 +5,34 @@ var roleFetcher = {
   /** @param {Creep} creep **/
   run: function (creep) {
     // Top priority: Dropped resources
-    const targets = creep.room.find(FIND_DROPPED_RESOURCES)
-    if (targets.length && creep.store.getFreeCapacity() > 0) {
-      // Pick up dropped resources
-      creep.say("🔄 dropped resources")
-      // console.log(`dropped resources at ${targets[0]}`)
-      creep.moveTo(targets[0], { visualizePathStyle: { stroke: "#ffaa00" } })
-      creep.pickup(targets[0])
+    const droppedResources = creep.room.find(FIND_DROPPED_RESOURCES)
+    if (droppedResources.length && creep.store.getFreeCapacity() > 0) {
+      if (creep.memory.droppedRedroppedResourceNumber == null) {
+        // Randomize current droppedResource assignment
+        creep.memory.droppedRedroppedResourceNumber = Math.floor(
+          Math.random() * droppedResources.length
+        )
+        creep.say("🔄 FILL UP")
+        console.log(
+          `${creep.name} assigned to @droppedResources[${creep.memory.droppedResourceNumber}]`
+        )
+      }
+      if (
+        creep.harvest(droppedResources[creep.memory.droppedResourceNumber]) ==
+        ERR_NOT_IN_RANGE
+      ) {
+        creep.moveTo(droppedResources[creep.memory.droppedResourceNumber], {
+          visualizePathStyle: { stroke: "#ffaa00" },
+        })
+      }
+      if (
+        creep.harvest(droppedResources[creep.memory.droppedResourceNumber]) ===
+        OK
+      ) {
+        // Log destination while harvesting
+        creep.memory.destination = { x: creep.pos.x, y: creep.pos.y }
+        creep.pickup(droppedResources[creep.memory.droppedResourceNumber])
+      }
     } else {
       if (creep.store.getUsedCapacity() > 0) {
         // Drop off resources
