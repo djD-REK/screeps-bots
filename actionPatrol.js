a788fd06420304f1d1b95102b525e3c5fc6f8a33
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-04-29 21:28:27 -0400

    Update actionPatrol.js

--- actionPatrol.js
+++ actionPatrol.js
@@ -1,24 +1,32 @@
+var actionExplore = require("actionExplore")
+
 function actionPatrol(creep) {
   // Patrol to a random flag
   var flags = creep.room.find(FIND_FLAGS)
-  if (creep.memory.flagNumber == null) {
-    // null or undefined
-    // Randomize current source assignment
-    creep.memory.flagNumber = Math.floor(Math.random() * flags.length)
-    creep.say("🚶 patrolling")
-    console.log(`${creep.name} assigned to @flags[${creep.memory.flagNumber}]`)
-  }
-  if (
-    flags[creep.memory.flagNumber].pos.x === creep.pos.x &&
-    flags[creep.memory.flagNumber].pos.y === creep.pos.y
-  ) {
-    console.log(`${creep.name} arrived at @flags[${creep.memory.flagNumber}]`)
-    creep.memory.flagNumber = null
+  if (flags.length > 0) {
+    if (creep.memory.flagNumber == null) {
+      // null or undefined
+      // Randomize current source assignment
+      creep.memory.flagNumber = Math.floor(Math.random() * flags.length)
+      creep.say("🚶 patrolling")
+      console.log(
+        `${creep.name} assigned to @flags[${creep.memory.flagNumber}]`
+      )
+    }
+    if (
+      flags[creep.memory.flagNumber].pos.x === creep.pos.x &&
+      flags[creep.memory.flagNumber].pos.y === creep.pos.y
+    ) {
+      console.log(`${creep.name} arrived at @flags[${creep.memory.flagNumber}]`)
+      creep.memory.flagNumber = null
+    } else {
+      // Get to moving
+      creep.moveTo(flags[creep.memory.flagNumber], {
+        visualizePathStyle: { stroke: "#ffaa00" },
+      })
+    }
   } else {
-    // Get to moving
-    creep.moveTo(flags[creep.memory.flagNumber], {
-      visualizePathStyle: { stroke: "#ffaa00" },
-    })
+    actionExplore(creep)
   }
 }
 
