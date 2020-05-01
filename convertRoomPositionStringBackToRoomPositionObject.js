9933434b8ca9f4d3f9b606f3140340442d120ee6
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-05-01 14:01:05 -0400

    Update convertRoomPositionStringBackToRoomPositionObject.js

--- convertRoomPositionStringBackToRoomPositionObject.js
+++ convertRoomPositionStringBackToRoomPositionObject.js
@@ -4,14 +4,17 @@ function convertRoomPositionStringBackToRoomPositionObject(stringRoomPosition) {
   // Using the constructor for to create a new RoomPosition object: constructor(x, y, roomName)
   // Output example: const pos = new RoomPosition(10, 25, 'sim');
   // --> from https://docs.screeps.com/api/#RoomPosition
-  const regExp = /room (?<room>\w{5}) pos (?<x>\d+),(?<y>\d+)/
+  const regExp = /room (?<roomName>\w{5}) pos (?<x>\d+),(?<y>\d+)/
   // Board is a 50x50 grid with coordinates ranging from 0 to 49 i.e. 1-2 digits
   const resultOfRegExp = regExp.exec(stringRoomPosition)
 
-  const roomName = resultOfRegExp.groups.room // e.g. E56N8
+  console.log(stringRoomPosition)
+
+  const roomName = resultOfRegExp.groups.roomName // e.g. E56N8
   const xCoordinate = Number(resultOfRegExp.groups.x) // e.g. 23
   const yCoordinate = Number(resultOfRegExp.groups.y) // e.g. 26
   return new RoomPosition(xCoordinate, yCoordinate, roomName) // e.g. new RoomPosition(23, 26, "E56N8")
 }
 
 module.exports = convertRoomPositionStringBackToRoomPositionObject
+// [room E56N8 pos 0,45]
