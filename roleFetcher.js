3523dfc29019b0728d535e5d7d66acf5fa9369ee
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-05-01 02:33:02 -0400

    Update roleFetcher.js

--- roleFetcher.js
+++ roleFetcher.js
@@ -60,10 +60,10 @@ var roleFetcher = {
 
       const droppedResources = thisCreep.room.find(FIND_DROPPED_RESOURCES, {
         filter: function (resource) {
-          return resource.amount >= 1 * carryingCapacity
+          return resource.amount >= 3 * carryingCapacity
         },
       })
-      console.log(droppedResources[0].amount)
+      // Target 3x carryingCapacity, i.e. big loads only
 
       if (droppedResources.length) {
         if (thisCreep.memory.droppedResourceNumber == null) {
