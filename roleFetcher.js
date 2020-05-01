329b4dc072052f0bbff130ecd98c85dda845a8ac
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-05-01 02:30:29 -0400

    Update roleFetcher.js

--- roleFetcher.js
+++ roleFetcher.js
@@ -60,6 +60,7 @@ var roleFetcher = {
 
       const droppedResources = thisCreep.room.find(FIND_DROPPED_RESOURCES, {
         filter: function (resource) {
+          console.log(`${resource.amount} vs. ${carryingCapacity}`)
           return resource.amount >= 1 * carryingCapacity
         },
       })
